4220e0fb4215662aa6aa70b724affdc69976c1ce
RGunny <sadog94@gmail.com>
2021-08-16 18:38:38 +0900

    [Java/DP] BOJ_1256 - 사전

--- src/boj/BOJ_1256_사전.java
+++ src/boj/BOJ_1256_사전.java
@@ -1,2 +1,72 @@
-package boj;public class BOJ_1256_사 {
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * 문자열, DP, 조합, 수학
+ * Gold 3
+ * 사전
+ * https://www.acmicpc.net/problem/1256
+ */
+public class BOJ_1256_사전 {
+    private static int N, M, K, num = 0, count = 0;
+    private static char result[];
+    private static boolean check[][];
+    private static long cnt[][];
+    private static long mod = 1000000001;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        cnt = new long[200 + 1][100 + 1];
+        check = new boolean[200 + 1][100 + 1];
+        result = new char[N + M + 1];
+
+        long num = binary(N + M, M);
+        if (num >= K) {
+            Arrays.fill(result, 'a');
+            find(N + M, M, K);
+            for (int i = 0; i < N + M; i++) sb.append(result[i]);
+        } else {
+            sb.append(-1);
+        }
+        System.out.println(sb);
+    }
+
+    private static long binary(int n, int m) {
+        if (n < m) return 0;
+        if (n == m || m == 0) return 1;
+        if (check[n][m]) return cnt[n][m];
+        check[n][m] = true;
+        cnt[n][m] += binary(n - 1, m);
+        cnt[n][m] += binary(n - 1, m - 1);
+        return Math.min(cnt[n][m], 1000000001);
+    }
+
+    private static void find(int n, int m, long k) {
+        if (n == 0) return;
+        if (m == 0) {
+            for (int i = 0; i < n; i++)
+                result[num + i] = 'a';
+            return;
+        }
+        long pivot = binary(n - 1, m);
+        if (pivot < k) {
+            result[num++] = 'z';
+            find(n - 1, m - 1, k - pivot);
+        } else {
+            result[num++] = 'a';
+            find(n - 1, m, k);
+        }
+    }
 }
--- src/boj/BOJ_1256_사전_V2.java
+++ src/boj/BOJ_1256_사전_V2.java
@@ -1,2 +1,60 @@
-package boj;public class BOJ_1256_사전_V2 {
+package boj;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_1256_사전_V2 {
+    private static long[][] dp;
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int N, M, K;
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        dp = new long[N + 1][M + 1];
+
+
+        if (K > numWord(N, M)) {
+            System.out.println("-1");
+        } else {
+            getWord(N, M, K - 1);
+        }
+        bw.write("\n");
+        bw.close();
+    }
+
+    private static long numWord(int N, int M) {
+        if (N == 0 || M == 0)
+            return 1;
+        if (dp[N][M] != 0)
+            return dp[N][M];
+        return dp[N][M] = Long.min(numWord(N - 1, M) + numWord(N, M - 1), 1000000001);
+    }
+
+    private static void getWord(int N, int M, long before) throws IOException {
+        if (N == 0) {
+            for (int i = 0; i < M; i++)
+                bw.write("z");
+            return;
+        }
+        if (M == 0) {
+            for (int i = 0; i < N; i++)
+                bw.write("a");
+            return;
+
+        }
+        long tmp = numWord(N - 1, M);
+
+        if (before < tmp) {
+            bw.write("a");
+            getWord(N - 1, M, before);
+        } else {
+            bw.write("z");
+            getWord(N, M - 1, before - tmp);
+        }
+    }
 }
