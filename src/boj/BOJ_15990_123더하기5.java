972846480531515679dc59f75d41c6316890eb41
RGunny <sadog94@gmail.com>
2021-09-30 21:57:28 +0900

    [Java/DP] BOJ_15990 - 1, 2, 3 더하기 5

--- src/boj/BOJ_15990_123더하기5.java
+++ src/boj/BOJ_15990_123더하기5.java
@@ -6,12 +6,13 @@ import java.io.InputStreamReader;
 
 /**
  * 다이나믹 프로그래밍
- * Silver 1
- * 1, 2, 3 더하기 4
- * https://www.acmicpc.net/problem/15989
+ * Silver 2
+ * 1, 2, 3 더하기 5
+ * https://www.acmicpc.net/problem/15990
  */
-public class BOJ_15989_123더하기4 {
-    private static long[][] dp = new long[10001][4];
+public class BOJ_15990_123더하기5 {
+    private static final int MOD = 1000000009;
+    private static long[][] dp = new long[100001][4];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,29 +20,29 @@ public class BOJ_15989_123더하기4 {
 
         int T = Integer.parseInt(br.readLine());
 
-        getDp();
+        make();
 
         while (T-- > 0) {
             int N = Integer.parseInt(br.readLine());
 
-            sb.append(dp[N][1] + dp[N][2] + dp[N][3] + "\n");
+            sb.append((dp[N][1] + dp[N][2] + dp[N][3]) % MOD + "\n");
         }
         System.out.println(sb.toString());
     }
 
-    private static void getDp() {
-
+    private static void make() {
+        
+        // 마지막 더한 숫자 기준 인덱스(1 ~ 3)
         dp[1][1] = 1; // 1
-        dp[2][1] = 1; // 1 + 1
         dp[2][2] = 1; // 2
-        dp[3][1] = 1; // 1 + 1 + 1
+        dp[3][1] = 1; // 2 + 1
         dp[3][2] = 1; // 1 + 2
         dp[3][3] = 1; // 3
 
-        for (int i = 4; i <= 10000; i++) {
-            dp[i][1] = dp[i - 1][1];
-            dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
-            dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
+        for (int i = 4; i <= 100000; i++) {
+            dp[i][1] = (dp[i - 1][2] + dp[i - 1][3]) % MOD;
+            dp[i][2] = (dp[i - 2][1] + dp[i - 2][3]) % MOD;
+            dp[i][3] = (dp[i - 3][1] + dp[i - 3][2]) % MOD;
         }
 
     }
