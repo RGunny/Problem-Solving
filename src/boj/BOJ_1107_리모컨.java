98d5b5cb2787167b56b3f8f7bb3c34e133091055
RGunny <sadog94@gmail.com>
2022-01-30 23:41:12 +0900

    [Java/BruteForce] BOJ_1107 - 리모컨

--- src/boj/BOJ_1107_리모컨.java
+++ src/boj/BOJ_1107_리모컨.java
@@ -1,2 +1,61 @@
-package boj;public class BOJ_1107_리모컨 {
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * Brute Force
+ * Gold 5
+ * 리모컨
+ * https://www.acmicpc.net/problem/1107
+ */
+public class BOJ_1107_리모컨 {
+    private static final int MAX_NUMBER = 999999;
+    private static int target, M;
+    private static boolean[] broken = new boolean[10];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        target = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine()); // 고장난 버튼의 개수
+
+        if (M != 0) {
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < M; i++)
+                broken[Integer.parseInt(st.nextToken())] = true;
+        }
+
+        int answer = solve();
+        System.out.println(answer);
+    }
+
+    private static int solve() {
+        // +, - 로만 누르는 경우 (최초 채널은 100) (모든 리모컨 버튼이 고장났을 경우)
+        int minNum = Math.abs(target - 100);
+
+        for (int i = 0; i <= MAX_NUMBER; i++) {
+            String str = String.valueOf(i);
+            int length = str.length();
+
+            boolean isBroken = false;
+            for (int j = 0; j < length; j++) {
+                // 고장난 버튼을 눌러야 하면
+                if (broken[str.charAt(j) - '0']) {
+                    isBroken = true;
+                    break;
+                }
+            }
+            // 고장나지 않은 버튼으로 목표와 가장 근사한 값을 생성
+            if (!isBroken) {
+                int min = length + Math.abs(target - i); // i(length)로 이동 후  target까지 이동하는 횟수 (+, -)
+                minNum = Math.min(min, minNum);
+            }
+        }
+        return minNum;
+    }
+
 }
--- src/boj/BOJ_2574_빙산.java
+++ src/boj/BOJ_2574_빙산.java
@@ -1,2 +1,135 @@
-package boj;public class BOJ_2574_빙산 {
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/**
+ * DFS, BFS
+ * Gold 4
+ * 빙산
+ * https://www.acmicpc.net/problem/2573
+ */
+public class BOJ_2574_빙산 {
+    private static final int[][] DIR = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // 상 하 좌 우
+    private static int N, M;
+    private static int[][] iceberg;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken()); // row
+        M = Integer.parseInt(st.nextToken()); // col
+
+        iceberg = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++)
+                iceberg[i][j] = Integer.parseInt(st.nextToken());
+        }
+
+        int time = solve();
+        System.out.println(time);
+    }
+
+    private static int solve() {
+        int time = 0;
+
+        while (true) {
+            // 1. 빙산이 녹는다. (동서남북 0의 갯수만큼)
+            // (차례대로 녹는 것이 아닌, 한 스탭에 한 번에 모두 녹아야 함) => 배열을 복제하여 해당 배열에서 비교한 후 원본 배열에서만 빙산을 녹임
+            melt();
+            time++;
+
+            // 2.1 두 덩이 이상의 빙산이 되었는 지 확인한다.
+            int theNumberOfIceberg = getIcebergNums();
+
+            // 2.2 두 덩이 이상이 되었다면, 해당 년을 반환하고 종료한다.
+            // 2.3 0덩이가 되었다면 실패하였으므로 0을 반환하고 종료한다.
+            if (theNumberOfIceberg > 1) {
+                break;
+            } else if (theNumberOfIceberg == 0) {
+                time = 0;
+                break;
+            }
+        }
+        return time;
+    }
+
+    private static void melt() {
+        int[][] copy = copy(iceberg);
+        Queue<int[]> q = new LinkedList<>();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (iceberg[i][j] <= 0) continue;
+
+                q.add(new int[]{i, j});
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int count = 0;
+            int[] cur = q.poll();
+
+            for (int d = 0; d < 4; d++) {
+                int nr = cur[0] + DIR[d][0];
+                int nc = cur[1] + DIR[d][1];
+
+                if (!isRange(nr, nc) || copy[nr][nc] > 0) continue;
+                count++;
+            }
+            iceberg[cur[0]][cur[1]] = iceberg[cur[0]][cur[1]] > count ? iceberg[cur[0]][cur[1]] - count : 0;
+        }
+    }
+
+    /*
+        기존 한 덩어리의 빙산이 두 개 이상의 빙산으로 분리되었는 지 판별하는 메서드
+     */
+    private static int getIcebergNums() {
+        boolean[][] isChecked = new boolean[N][M];
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (isChecked[i][j] || iceberg[i][j] < 1) continue;
+
+                dfs(isChecked, i, j);
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static void dfs(boolean[][] isChecked, int r, int c) {
+        isChecked[r][c] = true;
+
+        for (int d = 0; d < 4; d++) {
+            int nr = r + DIR[d][0];
+            int nc = c + DIR[d][1];
+
+            if (!isRange(nr, nc) || isChecked[nr][nc] || iceberg[nr][nc] < 1) continue;
+
+            dfs(isChecked, nr, nc);
+        }
+    }
+
+    private static int[][] copy(int[][] origin) {
+        int[][] copy = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++)
+                copy[i][j] = origin[i][j];
+        }
+        return copy;
+    }
+
+    private static boolean isRange(int nr, int nc) {
+        return nr >= 0 && nr < N && nc >= 0 && nc < M;
+    }
 }
