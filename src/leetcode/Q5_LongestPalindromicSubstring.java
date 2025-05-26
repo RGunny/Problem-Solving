66ae9400563a5823210fe50a91e846567f2e50dd
RGunny <sadog94@gmail.com>
2025-05-26 21:19:40 +0900

    [leetcode] : 5. Longest Palindromic Substring

---
+++ src/leetcode/Q5_LongestPalindromicSubstring.java
@@ -0,0 +1,65 @@
+package leetcode;
+
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * https://leetcode.com/problems/longest-palindromic-substring/
+ */
+public class Q5_LongestPalindromicSubstring {
+
+    public static void main(String[] args) {
+        runTests(new Q5_LongestPalindromicSubstring()::longestPalindrome);
+    }
+
+    public static void runTests(Function<String, String> solution) {
+        assertEquals("bb", solution.apply("cbbd"));
+        assertEquals("a", solution.apply("a"));
+        assertEquals("aaaa", solution.apply("aaaa"));
+        assertEquals("anana", solution.apply("bananas"));
+        assertEquals("", solution.apply(""));
+
+        String r1 = solution.apply("babad");
+        assertTrue(r1.equals("bab") || r1.equals("aba"));
+
+        String r2 = solution.apply("abcd");
+        assertEquals(1, r2.length());
+        assertTrue("abcd".contains(r2));
+
+        assertEquals("racecar", solution.apply("qracecarq"));
+    }
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            // 중심이 한 문자 (aba)
+            int length1 = expandFromCenter(s, i);
+            // 중심이 두 문자 (abba)
+            int length2 = expandFromCenter(s, i + 1);
+
+            int length = Math.max(length1, length2);
+
+            if (length > end - start) {
+                start = i - (length - 1) / 2;
+                end = i + length / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private static int expandFromCenter(String s, int i) {
+        int left = i, right = i;
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+}
+
